57ff7c579645daa4d0f8102fac0a51379ec96795
AndresN <camilonunezz9812@gmail.com>
2021-07-24 21:13:59 -0500

    Refactoring code adding functions for repeated components CarouselItem in list elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,11 @@ import useAPI from '../hooks/useAPI';
 
 function Home() {
 
-    const chaptersMyList = Array.from({length: 10}, () => Math.floor(Math.random() * 671));
-    const chaptersTop3 = Array.from({length: 5}, () => Math.floor(Math.random() * 671));
-    const chaptersRecommended = Array.from({length: 20}, () => Math.floor(Math.random() * 671));
+    const arrayGenerator = (len) => Array.from({length: len}, () => Math.floor(Math.random() * 671));
+
+    const chaptersMyList = arrayGenerator(10);
+    const chaptersTop3 = arrayGenerator(3);
+    const chaptersRecommended = arrayGenerator(20);
 
     const coversAPI = (ids) => `https://rickandmortyapi.com/api/character/${ids}`;
 
@@ -27,52 +29,34 @@ function Home() {
         )
     }
 
+    const liCarouselItem = (arrayCovers) => (
+        (arrayCovers.length)
+        ?
+        arrayCovers.map(cover => (
+            <li key={cover.id} className="Carousel__list-element">
+                <CarouselItem data={cover} />
+            </li>
+        ))
+        :
+        <p>Error</p>
+    )
+
     return (
         <React.Fragment>
             <Search />
             <Carousel title="My lista">
                 <ul className="Carousel__list">
-                    {
-                        (coversMyList.length)
-                        ?
-                        coversMyList.map(cover => (
-                            <li key={cover.id} className="Carousel__list-element">
-                                <CarouselItem data={cover} />
-                            </li>
-                        ))
-                        :
-                        (<p>Hola</p>)
-                    }
+                    {liCarouselItem(coversMyList)}
                 </ul>
             </Carousel>
             <Carousel title="Recommended">
                 <ul className="Carousel__list">
-                    {
-                        (coversRecommended.length)
-                        ?
-                        coversRecommended.map(cover => (
-                            <li key={cover.id} className="Carousel__list-element">
-                                <CarouselItem data={cover} />
-                            </li>
-                        ))
-                        :
-                        (<p>Hola</p>)
-                    }
+                    {liCarouselItem(coversRecommended)}
                 </ul>
             </Carousel>
             <Carousel title="Top 5">
                 <ul className="Carousel__list">
-                    {
-                        (coversTop3.length)
-                        ?
-                        coversTop3.map(cover => (
-                            <li key={cover.id} className="Carousel__list-element">
-                                <CarouselItem data={cover} />
-                            </li>
-                        ))
-                        :
-                        (<p>Hola</p>)
-                    }
+                    {liCarouselItem(coversTop3)}
                 </ul>
             </Carousel>
         </React.Fragment>
