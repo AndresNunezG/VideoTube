ba66774f7634d421dc35c39d0615274c2a976728
AndresN <camilonunezz9812@gmail.com>
2021-07-25 19:53:15 -0500

    like option and styles added to CarouselItem elements

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../assets/styles/components/CarouselItem.scss'
 
-const CarouselItem = (props) => (
-    <div className="Item__container">
-        <img className="Item__img" src={props.data.image} alt="item image" />
-        <div className="Item__details-container">
-            <div>
-                <i className="Item__icon-play far fa-heart"></i>
+const CarouselItem = (props) => {
+    const [like, setLike] = useState(false);
+    const handleClick = () => {
+        setLike(!like);
+    };
+    return(
+        <div className="Item__container">
+            <img className="Item__img" src={props.data.image} alt="item image" />
+            <div className="Item__details-container">
+                <div>
+                    <i onClick={handleClick} className={(!like ? "Item__icon-play far fa-heart": "Item__icon-play fas fa-heart")}></i>
+                </div>
+                <p className="Item__details-title">{props.data.name}</p>
+                <p className="Item__details-subtitle">{props.data.status} - {props.data.origin.name}</p>
             </div>
-            <p className="Item__details-title">{props.data.name}</p>
-            <p className="Item__details-subtitle">{props.data.status} - {props.data.origin.name}</p>
         </div>
-    </div>
-);
+    )
+};
 
 export default CarouselItem;
