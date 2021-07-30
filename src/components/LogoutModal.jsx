fac820bfe2a44d36b6bbadde6ede14336c0b6951
AndresN <camilonunezz9812@gmail.com>
2021-07-30 17:11:33 -0500

    Logout model complete

--- src/components/LogoutModal.jsx
+++ src/components/LogoutModal.jsx
@@ -3,14 +3,20 @@ import { Link } from 'react-router-dom';
 
 import Modal from './Modal';
 
+import '../assets/styles/components/LogoutModal.scss'
+
 const LogoutModal = (props) => {
     return (
         <Modal isOpen={props.isOpen} onCloseModal={props.onCloseModal}>
-            <h3>You're about to be signed out of VideoTube</h3>
-            <p>You'll be redirected to the home page</p>
-            <Link to="/">
-                <button type="button" className="Button__continue">Continue</button>
-            </Link>
+            <div className="Logout__container">
+                <h3 className="Logout__title">You're about to be signed out of VideoTube</h3>
+                <p className="Logout__subtitle">You'll be redirected to the home page</p>
+                <div className="Button__container">
+                    <Link to="/">
+                        <button type="button" className="Button__continue">Continue</button>
+                    </Link>
+                </div>
+            </div>
         </Modal>
     );
 }
