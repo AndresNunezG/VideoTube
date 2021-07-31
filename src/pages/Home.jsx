aee236e1091b7a5971c7f50823bab44934221df0
AndresN <camilonunezz9812@gmail.com>
2021-07-31 18:58:06 -0500

    login data pushed to history handleChange and handleClick added to form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,32 @@ import ImgWave2 from '../assets/static/ImgWave2.svg'
 import '../assets/styles/Home.scss'
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            age: '',
+            email: '',
+        }
+    }
+    handleClick = (e) => {
+        e.preventDefault();
+        if (this.state.name && this.state.age && this.state.email) {
+            this.props.history.push({
+                pathname: '/browse',
+                state: {...this.state}
+            });
+        }
+        else {
+            alert('campo vacio')
+        }
+    }
+    handleChange = (e) => {
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value,
+        })
+    }
     render () {
         return (
             <div className="Home__container">
@@ -18,18 +44,38 @@ class Home extends React.Component {
                         <form className="LoginForm">
                             <div className="Field__container Field-name">
                                 <label className="Field-label" htmlFor="name">Name</label>
-                                <input className="Field-input" name="name" id="name"></input>
+                                <input
+                                    onChange={this.handleChange}
+                                    value={this.state.name}
+                                    className="Field-input"
+                                    name="name"
+                                    id="name">
+                                </input>
                             </div>
                             <div className="Field__container Field-email">
                                 <label className="Field-label" htmlFor="email">Email</label>
-                                <input className="Field-input" type="email" min="0" name="email" id="email"></input>
+                                <input
+                                    onChange={this.handleChange}
+                                    className="Field-input"
+                                    value={this.state.email}
+                                    type="email"
+                                    name="email"
+                                    id="email">
+                                </input>
                             </div>
                             <div className="Field__container Field-age">
                                 <label className="Field-label" htmlFor="age">Age</label>
-                                <input className="Field-input" type="number" min="0" max="100" name="age" id="age"></input>
+                                <input
+                                    onChange={this.handleChange}
+                                    className="Field-input"
+                                    value={this.state.age}
+                                    type="number" min="0"
+                                    max="100" name="age"
+                                    id="age">
+                                </input>
                             </div>
                             <div className="Button__container">
-                                <button className="Button__login">Continue</button>
+                                <button onClick={this.handleClick} type="button" className="Button__login">Continue</button>
                             </div>
                         </form>
                     </div>
